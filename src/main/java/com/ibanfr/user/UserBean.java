d1fa6be4a9c41a60f66fff00152f3fb79cbced37
ivan.fernandez <ivan.fernandez@empowertheuser.com>
2018-07-12 17:32:24 +0100

    user list test

---
+++ src/main/java/com/ibanfr/user/UserBean.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.ibanfr.user;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+import com.ibanfr.hibernate.model.User;
+import com.ibanfr.manager.UserManager5;
+
+
+/**
+ * @author ivan
+ *
+ */
+@ManagedBean
+@SessionScoped
+public class UserBean {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserBean.class);
+	
+	private static Marker APPEND_IBANFR = MarkerFactory.getMarker("APPEND_IBANFR");
+	
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		
+		logger.debug(APPEND_IBANFR, "Append some text to the log File");
+		
+		try {
+
+			UserManager5 manager = new UserManager5();
+			setUsers(manager.listAllUsers());
+				
+		}catch(Exception e) {
+			logger.error("There was an error retrieving the users", e);
+		}
+		
+	}
+	
+	
+
+}
