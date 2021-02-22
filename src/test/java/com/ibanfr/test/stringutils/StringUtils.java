b80df641c3542f5684dd24874902dc0dc46ddffe
ivan.fernandez <ivan.fernandez@empowertheuser.com>
2021-02-22 16:22:17 +0100

    test updates

--- src/test/java/com/ibanfr/test/stringutils/StringUtils.java
+++ src/test/java/com/ibanfr/test/stringutils/StringUtils.java
@@ -3,10 +3,10 @@
  */
 package com.ibanfr.test.stringutils;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
-
-
-import com.google.common.collect.Lists;
 
 /**
  * @author ivan
@@ -14,12 +14,47 @@ import com.google.common.collect.Lists;
  */
 public class StringUtils {
 
+	@Test
+	public void testStringUtils1() {
+		
+		List<Integer> listIntegers = new ArrayList<>();
+		listIntegers.add(1);
+		listIntegers.add(3);
+		System.out.println(org.apache.commons.lang3.StringUtils.join(listIntegers,","));
+	}
+	
+	@Test
+	public void testStringUtils2() {
+		
+		List<Integer> listIntegers = null;
+		System.out.println(org.apache.commons.lang3.StringUtils.join(listIntegers,","));
+	}
+	
+	@Test
+	public void testStringFormat1() {
+		
+		List<Integer> listIntegers = null;
+		System.out.println(String.format("this is a list of integers=%s", listIntegers));
+	}
+	
+	@Test
+	public void testStringFormat2() {
+		
+		List<Integer> listIntegers = new ArrayList<>();
+		listIntegers.add(1);
+		listIntegers.add(3);
+		System.out.println(String.format("this is a list of integers=%s", listIntegers));
+	}
+	
+	
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> listIntegers = Lists.newArrayList();
+		List<Integer> listIntegers = new ArrayList<>();
 		listIntegers.add(1);
 		listIntegers.add(3);
 		System.out.println(org.apache.commons.lang3.StringUtils.join(listIntegers,","));
